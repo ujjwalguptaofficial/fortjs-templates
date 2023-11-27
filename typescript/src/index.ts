805899db97732552d53a4db3448ed85a22a3a8d2
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2023-11-27 17:37:55 +0400

    change debug message

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -15,7 +15,7 @@ export const createApp = async () => {
 };
 if (process.env.NODE_ENV !== "test") {
     createApp().then(() => {
-        Fort.logger.debug(`Your fort is located at address - ${process.env.APP_URL}`);
+        Fort.logger.debug(`Your fort has been forged and is now ready for exploration at ${process.env.APP_URL}`);
     }).catch(err => {
         console.error(err);
     });
